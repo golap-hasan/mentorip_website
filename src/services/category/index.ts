35a02b6760c1ba83f1c607cc9c9bac48e87c5e0a
nurulla-hasan <nurullahasan.dev@gmail.com>
2026-01-13 16:38:35 +0600

    add revalidation

--- src/services/category/index.ts
+++ src/services/category/index.ts
@@ -10,6 +10,7 @@ export const getAllCategories = async (): Promise<ApiResponse<Category[]>> => {
   try {
     const result = await serverFetch('/category', {
       method: 'GET',
+      revalidate: 3600,
     });
     return result;
   } catch (error: any) {
@@ -17,27 +18,11 @@ export const getAllCategories = async (): Promise<ApiResponse<Category[]>> => {
   }
 };
 
-// GET ALL CATEGORIES WITH POST COUNT
-export const getCategoriesWithCount = async () => {
-  try {
-    return await serverFetch(`/category/with-post-count`, {
-      revalidate: 300,
-      tags: ["CATEGORY-LIST", "POST-COUNT"],
-    });
-  } catch {
-    return {
-      success: false,
-      data: [],
-    };
-  }
-};
-
 // GET SINGLE CATEGORY WITH POSTS (by slug)
 export const getCategoryBySlug = async (slug: string, query: Query = {}): Promise<ApiResponse<CategoryWithPosts | null>> => {
   try {
     return await serverFetch(`/category/with-posts/${slug}${buildQueryString(query)}`, {
-      revalidate: 300,
-      tags: [`CATEGORY-DETAIL-${slug}`],
+      revalidate: 3600,
     });
   } catch {
     return {
